c497c7c06540e3e2f592881f6a0ec5ae7198423b
Hieu Nguyen <hieub1809236@student.ctu.edu.vn>
2021-08-12 21:27:43 +0700

    add react lazy and suspense for app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
+import { fakeUpdateUser } from "app/userSlice";
+import Loading from "components/Loading";
 import NotFound from "components/NotFound";
 import ProtectedRoute from "components/ProtectedRoute";
-import Cart from "features/Cart";
-import Home from "features/Home";
-import Order from "features/Order";
+import firebase from "firebase";
+import React, { Suspense, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.scss";
-
-import firebase from "firebase";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fakeUpdateUser } from "app/userSlice";
 
 // Configure Firebase.
 const config = {
@@ -28,24 +25,32 @@ function App() {
     const unregisterAuthObserver = firebase
       .auth()
       .onAuthStateChanged(async (user) => {
-        const token = await user.getIdToken();
-        const image = user.photoURL;
-        dispatch(fakeUpdateUser({ token, user: { image, test: "test" } }));
+        if (user) {
+          const token = await user.getIdToken();
+          const image = user.photoURL;
+          dispatch(fakeUpdateUser({ token, user: { image, test: "test" } }));
+        }
       });
     return () => unregisterAuthObserver();
   }, []);
 
+  const Home = React.lazy(() => import("./features/Home"));
+  const Cart = React.lazy(() => import("./features/Cart"));
+  const Order = React.lazy(() => import("./features/Order"));
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/cart" component={Cart} />
-          <ProtectedRoute path="/order" component={Order} />
-          <Route component={NotFound} />
-        </Switch>
-        <ToastContainer />
-      </BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/cart" component={Cart} />
+            <ProtectedRoute path="/order" component={Order} />
+            <Route component={NotFound} />
+          </Switch>
+          <ToastContainer />
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
